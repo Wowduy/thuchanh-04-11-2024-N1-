08aab5527cff671316182c9c69139dc6a84f76eb
Wowduy <161957830+Wowduy@users.noreply.github.com>
2024-11-04 16:14:50 +0700

    Add files via upload

---
+++ Thực hành 04112024 (N1)/Thực hành 04112024 (N1)/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Thực_hành_04112024__N1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.dgvServices = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDeleteCustomer = new System.Windows.Forms.Button();
+            this.btnEditCustomer = new System.Windows.Forms.Button();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.btnCreateInvoice = new System.Windows.Forms.Button();
+            this.lblTotalAmountLabel = new System.Windows.Forms.Label();
+            this.lvSelectedServices = new System.Windows.Forms.ListView();
+            this.madv = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tendv = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.giatien = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAddServiceToInvoice = new System.Windows.Forms.Button();
+            this.lblTotalAmount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(-23, 216);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(449, 227);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // dgvServices
+            // 
+            this.dgvServices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvServices.Location = new System.Drawing.Point(630, 43);
+            this.dgvServices.Name = "dgvServices";
+            this.dgvServices.Size = new System.Drawing.Size(349, 134);
+            this.dgvServices.TabIndex = 1;
+            this.dgvServices.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvServices_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ten";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnDeleteCustomer);
+            this.groupBox1.Controls.Add(this.btnEditCustomer);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAddCustomer);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dgvCustomers);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 551);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(165, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "dia chi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "sdt";
+            // 
+            // btnDeleteCustomer
+            // 
+            this.btnDeleteCustomer.Location = new System.Drawing.Point(293, 473);
+            this.btnDeleteCustomer.Name = "btnDeleteCustomer";
+            this.btnDeleteCustomer.Size = new System.Drawing.Size(40, 55);
+            this.btnDeleteCustomer.TabIndex = 3;
+            this.btnDeleteCustomer.Text = "xoa";
+            this.btnDeleteCustomer.UseVisualStyleBackColor = true;
+            this.btnDeleteCustomer.Click += new System.EventHandler(this.btnDeleteCustomer_Click);
+            // 
+            // btnEditCustomer
+            // 
+            this.btnEditCustomer.Location = new System.Drawing.Point(176, 473);
+            this.btnEditCustomer.Name = "btnEditCustomer";
+            this.btnEditCustomer.Size = new System.Drawing.Size(42, 55);
+            this.btnEditCustomer.TabIndex = 2;
+            this.btnEditCustomer.Text = "sua";
+            this.btnEditCustomer.UseVisualStyleBackColor = true;
+            this.btnEditCustomer.Click += new System.EventHandler(this.btnEditCustomer_Click);
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(65, 473);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(41, 55);
+            this.btnAddCustomer.TabIndex = 1;
+            this.btnAddCustomer.Text = "them";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // btnCreateInvoice
+            // 
+            this.btnCreateInvoice.Location = new System.Drawing.Point(630, 475);
+            this.btnCreateInvoice.Name = "btnCreateInvoice";
+            this.btnCreateInvoice.Size = new System.Drawing.Size(349, 75);
+            this.btnCreateInvoice.TabIndex = 7;
+            this.btnCreateInvoice.Text = "tao hoa don";
+            this.btnCreateInvoice.UseVisualStyleBackColor = true;
+            this.btnCreateInvoice.Click += new System.EventHandler(this.btnCreateInvoice_Click);
+            // 
+            // lblTotalAmountLabel
+            // 
+            this.lblTotalAmountLabel.AutoSize = true;
+            this.lblTotalAmountLabel.Location = new System.Drawing.Point(627, 406);
+            this.lblTotalAmountLabel.Name = "lblTotalAmountLabel";
+            this.lblTotalAmountLabel.Size = new System.Drawing.Size(90, 13);
+            this.lblTotalAmountLabel.TabIndex = 8;
+            this.lblTotalAmountLabel.Text = "tong tien hoa don";
+            // 
+            // lvSelectedServices
+            // 
+            this.lvSelectedServices.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.madv,
+            this.tendv,
+            this.giatien});
+            this.lvSelectedServices.HideSelection = false;
+            this.lvSelectedServices.Location = new System.Drawing.Point(628, 263);
+            this.lvSelectedServices.Name = "lvSelectedServices";
+            this.lvSelectedServices.Size = new System.Drawing.Size(349, 97);
+            this.lvSelectedServices.TabIndex = 10;
+            this.lvSelectedServices.UseCompatibleStateImageBehavior = false;
+            this.lvSelectedServices.View = System.Windows.Forms.View.Details;
+            // 
+            // madv
+            // 
+            this.madv.Text = "madv";
+            // 
+            // tendv
+            // 
+            this.tendv.Text = "tendv";
+            // 
+            // giatien
+            // 
+            this.giatien.Text = "giatien";
+            // 
+            // btnAddServiceToInvoice
+            // 
+            this.btnAddServiceToInvoice.Location = new System.Drawing.Point(628, 192);
+            this.btnAddServiceToInvoice.Name = "btnAddServiceToInvoice";
+            this.btnAddServiceToInvoice.Size = new System.Drawing.Size(347, 23);
+            this.btnAddServiceToInvoice.TabIndex = 11;
+            this.btnAddServiceToInvoice.Text = "them vao hoa don";
+            this.btnAddServiceToInvoice.UseVisualStyleBackColor = true;
+            this.btnAddServiceToInvoice.Click += new System.EventHandler(this.btnAddServiceToInvoice_Click);
+            // 
+            // lblTotalAmount
+            // 
+            this.lblTotalAmount.AutoSize = true;
+            this.lblTotalAmount.Location = new System.Drawing.Point(740, 406);
+            this.lblTotalAmount.Name = "lblTotalAmount";
+            this.lblTotalAmount.Size = new System.Drawing.Size(39, 13);
+            this.lblTotalAmount.TabIndex = 12;
+            this.lblTotalAmount.Text = "0 VND";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 564);
+            this.Controls.Add(this.lblTotalAmount);
+            this.Controls.Add(this.btnAddServiceToInvoice);
+            this.Controls.Add(this.lvSelectedServices);
+            this.Controls.Add(this.lblTotalAmountLabel);
+            this.Controls.Add(this.btnCreateInvoice);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvServices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.DataGridView dgvServices;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDeleteCustomer;
+        private System.Windows.Forms.Button btnEditCustomer;
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreateInvoice;
+        private System.Windows.Forms.Label lblTotalAmountLabel;
+        private System.Windows.Forms.ListView lvSelectedServices;
+        private System.Windows.Forms.ColumnHeader madv;
+        private System.Windows.Forms.ColumnHeader tendv;
+        private System.Windows.Forms.ColumnHeader giatien;
+        private System.Windows.Forms.Button btnAddServiceToInvoice;
+        private System.Windows.Forms.Label lblTotalAmount;
+    }
+}
+
---
+++ Thực hành 04112024 (N1)/Thực hành 04112024 (N1)/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thực_hành_04112024__N1_
+{
+    public partial class Form1 : Form
+    {
+        private List<Customer> customers = new List<Customer>();
+        private List<Service> services = new List<Service>();
+        private List<Service> selectedServices = new List<Service>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeData();
+            LoadCustomersToGrid();
+            LoadServicesToGrid();
+        }
+
+        private void InitializeData()
+        {
+            customers.Add(new Customer { Id = 1, Name = "Nguyễn Văn A", Phone = "0123456789", Address = "Hà Nội" });
+            customers.Add(new Customer { Id = 2, Name = "Trần Thị B", Phone = "0987654321", Address = "TP Hồ Chí Minh" });
+
+            services.Add(new Service { Code = "DV01", Name = "Dịch vụ 1", Price = 100000 });
+            services.Add(new Service { Code = "DV02", Name = "Dịch vụ 2", Price = 200000 });
+            services.Add(new Service { Code = "DV03", Name = "Dịch vụ 3", Price = 300000 });
+        }
+
+        // Tải danh sách khách hàng lên DataGridView
+        private void LoadCustomersToGrid()
+        {
+            dgvCustomers.DataSource = null;
+            dgvCustomers.DataSource = customers;
+            dgvCustomers.Refresh();
+        }
+
+        // Tải danh sách dịch vụ lên DataGridView
+        private void LoadServicesToGrid()
+        {
+            dgvServices.DataSource = null;
+            dgvServices.DataSource = services;
+        }
+
+        // Thêm khách hàng mới
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+           
+            Customer duy = new Customer()
+            {
+                Id = 4,
+                Name = textBox1.Text,
+                Phone = textBox2.Text,
+                Address = textBox3.Text
+            };
+            customers.Add(duy);
+            dgvCustomers.DataSource = null;
+            dgvCustomers.DataSource = customers;
+            dgvCustomers.Refresh();
+        }
+
+        // Chỉnh sửa thông tin khách hàng
+        private void btnEditCustomer_Click(object sender, EventArgs e)
+        {
+            if (dgvCustomers.SelectedRows.Count > 0)
+            {
+                var selectedCustomer = (Customer)dgvCustomers.SelectedRows[0].DataBoundItem;
+                selectedCustomer.Name = "Tên mới đã sửa";
+                selectedCustomer.Phone = "SĐT mới đã sửa";
+                selectedCustomer.Address = "Địa chỉ mới đã sửa";
+            }
+        }
+
+        // Xóa khách hàng
+        private void btnDeleteCustomer_Click(object sender, EventArgs e)
+        {
+            if (dgvCustomers.CurrentRow != null )
+            {
+                int s = dgvCustomers.CurrentRow.Index;
+                customers.RemoveAt(s);
+                LoadCustomersToGrid();
+            }
+        }
+
+        // Thêm dịch vụ vào hóa đơn
+        private void btnAddServiceToInvoice_Click(object sender, EventArgs e)
+        {
+            if (dgvServices.SelectedRows.Count > 0)
+            {
+                var selectedService = (Service)dgvServices.SelectedRows[0].DataBoundItem;
+                selectedServices.Add(selectedService);
+
+                // Hiển thị dịch vụ đã chọn vào ListView
+                ListViewItem item = new ListViewItem(selectedService.Code);
+                item.SubItems.Add(selectedService.Name);
+                item.SubItems.Add(selectedService.Price.ToString("C"));
+                lvSelectedServices.Items.Add(item);
+
+                // Cập nhật tổng tiền
+                UpdateTotalAmount();
+            }
+        }
+
+        // Tính và hiển thị tổng tiền hóa đơn
+        private void UpdateTotalAmount()
+        {
+            decimal totalAmount = selectedServices.Sum(service => service.Price);
+            lblTotalAmount.Text = $"Tổng tiền: {totalAmount:C}";
+        }
+
+        // Tạo hóa đơn
+        private void btnCreateInvoice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvServices_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    // Lớp Khách hàng
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+
+    // Lớp Dịch vụ
+    public class Service
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
